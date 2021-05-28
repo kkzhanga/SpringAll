5622c0c33d0e2caecfb53ee57d84be6e31460721
zhangkk <zhangkk@deepblueai.com>
2021-05-28 11:25:39 +0800

    spring sourcecode demo1

---
+++ 00.Small-Spring/Source-Code-01/src/main/java/cn/kk/springframework/BeanDefinition.java
@@ -0,0 +1,16 @@
+package cn.kk.springframework;
+
+
+public class BeanDefinition {
+
+    private Object bean;
+
+    public BeanDefinition(Object bean) {
+        this.bean = bean;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
+
+}
